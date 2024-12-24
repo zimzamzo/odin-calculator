a187f8983f1fd85cbbe56c1495b308782bdb5713
Alan H <190987223+zimzamzo@users.noreply.github.com>
2024-12-24 14:59:54 -0600

    Implement reset function and display updater

--- script.js
+++ script.js
@@ -3,6 +3,8 @@ let newNum = 0;
 let display ="0";
 let recentPress = null;
 let recentOperator = null;
+const operatorsString = "+-*/";
+const numbersString = "0123456789";
 
 const add = (a,b) => a+b;
 const subtract = (a,b) => a-b;
@@ -23,7 +25,43 @@ function appendValue(value) {
 
 }
 
+function updateDisplay() {
+    document.querySelector(".display").textContent = newNum;
+}
+
+function resetCalculator() {
+    savedNum = null;
+    newNum = 0;
+    display ="0";
+    recentPress = null;
+    recentOperator = null;
+
+    updateDisplay();
+}
+
 function buttonEvent(button) {
+
+    if (operatorsString.includes(button)) {
+
+
+
+    }
+    
+    else if (numbersString.includes(button)) {
+
+        appendValue(button);
+
+    }
+    
+    else if (button == "=") {
+        
+
+
+    }
+
+    else if (button == "C") {
+        resetCalculator();
+    }
 
 }
 
@@ -33,3 +71,5 @@ document.querySelector(".buttons-container").addEventListener("click", (e) => {
         buttonEvent(buttonChoice);
     }
 });
+
+updateDisplay();
