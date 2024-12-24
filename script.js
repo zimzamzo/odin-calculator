f9bd12f73ca57b52d125af8307567bbd2a1d05f6
Alan H <190987223+zimzamzo@users.noreply.github.com>
2024-12-24 16:00:26 -0600

    Implement logic for updating value and display

--- script.js
+++ script.js
@@ -1,8 +1,7 @@
 let savedNum = null;
-let newNum = 0;
-let display ="0";
-let recentPress = null;
+let currentNum = 0;
 let recentOperator = null;
+let recentPress = null;
 const operatorsString = "+-*/";
 const numbersString = "0123456789";
 
@@ -21,47 +20,63 @@ function operate(operator,num1,num2) {
 }
 
 // update num variables and display
-function appendValue(value) {
-
+function appendValue(num) {
+    if (currentNum === 0) currentNum = +num;
+    else currentNum = +(`${currentNum}` + num);
 }
 
 function updateDisplay() {
-    document.querySelector(".display").textContent = newNum;
+    document.querySelector(".display").textContent = currentNum;
 }
 
 function resetCalculator() {
     savedNum = null;
-    newNum = 0;
-    display ="0";
+    currentNum = 0;
     recentPress = null;
     recentOperator = null;
-
-    updateDisplay();
 }
 
 function buttonEvent(button) {
 
     if (operatorsString.includes(button)) {
 
+        if (savedNum) {
+            currentNum = operate(recentOperator,savedNum,currentNum);
+        }
 
-
+        recentOperator = button;
     }
     
     else if (numbersString.includes(button)) {
 
-        appendValue(button);
+        // selected an operator and just pressed it
+        if (recentOperator && operatorsString.includes(recentPress)) {
+            savedNum = currentNum;
+            currentNum = 0;
+        }
+
+        // don't do anything if display is 0 and 0 pressed
+        if (!(button==="0" && currentNum===0)) appendValue(button);
 
     }
     
-    else if (button == "=") {
+    else if (button === "=") {
         
-
+        if (recentOperator && savedNum) {
+            currentNum = operate(recentOperator,savedNum,currentNum);
+            savedNum = null;
+            recentOperator = null;
+        }
 
     }
 
-    else if (button == "C") {
+    else if (button === "C") {
         resetCalculator();
     }
+
+    
+    if (button !== "C") recentPress = button;
+    updateDisplay();
 
 }
 
